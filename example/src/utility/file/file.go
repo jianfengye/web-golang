2279d66c7328ae09875e677f18827e4bee2d4192
czyang <coding.game@gmail.com>
2012-11-19 00:18:29 +0800

    Update example/src/utility/file/file.go
    WalkFunc need return a error when something wrong, such as read in a wrong directory. 

--- example/src/utility/file/file.go
+++ example/src/utility/file/file.go
@@ -9,6 +9,9 @@ func ReadAllFiles(dirname string)(map[string]os.FileInfo, error) {
 	list := make(map[string]os.FileInfo, 0)
 
 	err := filepath.Walk(dirname, func(path string, f os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 		list[path] = f
 		return nil	
 	})
